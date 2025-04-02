494b3df5c090c429cd452a5b48b58de345e066e8
Christian Conway <165970596+Lazy-Stormz@users.noreply.github.com>
2025-04-02 01:40:15 -0500

    Making Scripts
    Made beginnings of the code for the puzzle and chess puzzles.

---
+++ HauntedMansionEscape/Assets/Scripts/PianoState.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PianoState
+{
+    IDLE,
+    _FIRST_KEY_E,
+    _SECOND_KEY_G,
+    _THIRD_KEY_A,
+    _FINAL_KEY_F,
+    ERROR,
+}

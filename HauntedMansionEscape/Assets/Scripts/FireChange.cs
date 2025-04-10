22ab3a169ac9a218c62c472518995313c2095e48
Quentin Quarles <quentavius115@gmail.com>
2025-04-10 06:31:18 -0500

    Added actual record recordings, added sound queue when king gets burned

--- HauntedMansionEscape/Assets/Scripts/FireChange.cs
+++ HauntedMansionEscape/Assets/Scripts/FireChange.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class FireChange : MonoBehaviour
 {
     public bool burned = false;
+    public AudioSource kingYell;
+    public GameObject King;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
 
+    }
     void OnTriggerEnter(Collider collision)
 	{
 		if (collision.gameObject.CompareTag("King"))
@@ -24,8 +25,10 @@ public class FireChange : MonoBehaviour
     private IEnumerator DelayedSignal()
 	{
 		//Wait for the delay
+        kingYell.Play();
 		yield return new WaitForSeconds(2);
         burned = true;
+        Destroy(King);
 
     }
 
